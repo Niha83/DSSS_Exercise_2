6d805b7ebe9c18ba34561b5ec7918c5c2d5e3208
Niha83 <73181166+Niha83@users.noreply.github.com>
2024-11-11 02:24:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="DSSS_Exercise_2", 
-    version="0.1",
+    name='math_quiz',
+    version='1.0.0',
     packages=find_packages(),
-    install_requires=['numpy'],  
-    entry_points={
-        "console_scripts": [
-            "my_command=DSSS_Exercise_2.main:main",  # Replace with your main function or script path
-        ]
-    },
-    author="Niha Nasir",
-    author_email="nihanasir83@gmail.com",
-    description="A simple math game",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+    install_requires=[
+        numpy
     ],
+    test_suite='tests',
+    entry_points={
+        'console_scripts': [
+            'math_quiz=math_quiz:main',
+        ],
+    },
 )
